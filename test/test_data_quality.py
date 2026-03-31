bacf6b5d11675cf9bea9b90461c86bb3081b1ab1
Martin Rey <m.rey00@gmail.com>
2026-03-31 14:59:24 -0300

    Inicio de la actividad de verificación de calidad de los datos. #8

---
+++ test/test_data_quality.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from pandera.pandas import DataFrameSchema, Column, Check
+import pytest
+
+@pytest.fixture
+def datos_creditos():
+    """ Función para cargar los datos de créditos desde un archivo CSV.
+    Returns:
+        pd.DataFrame: DataFrame con los datos de créditos.
+    """
+    df = pd.read_csv("data/raw/datos_creditos.csv", sep=";")
+    return df
+
+
+@pytest.fixture
+def datos_tarjetas():
+    """ Función para cargar los datos de tarjetas desde un archivo CSV.
+    Returns:
+        pd.DataFrame: DataFrame con los datos de tarjetas.
+    """
+    df = pd.read_csv("data/raw/datos_tarjetas.csv", sep=";")
+    return df
+
+
+def test_esquema_datos_creditos(datos_creditos):
+    """ Prueba para validar el esquema de los datos de créditos.
+    Args:
+        datos_creditos (pd.DataFrame): DataFrame con los datos de créditos.
+    """
+    esquema = DataFrameSchema({
+        "id_cliente": Column(int, nullable=False),
+        "edad": Column(int, Check.greater_than_or_equal_to(18)),
+        "ingresos": Column(float, Check.greater_than_or_equal_to(0)),
+        "falta_pago": Column(int, nullable=False)
+    })
+    esquema.validate(datos_creditos)
